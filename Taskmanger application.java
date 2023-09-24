08c74e816a9872edd2be2fd361059cb6803423e5
K Yaswanth <140515489+Yaswanth270@users.noreply.github.com>
2023-09-24 22:17:40 +0530

    Create Taskmanger application.java

---
+++ Taskmanger application.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class TaskManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerApplication.class, args);
+    }
+}
